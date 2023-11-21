8422c8f8b1cdb7e16c075c95918b085fd8edaa3d
Elise Erickson <elisece717@gmail.com>
2023-11-21 16:37:36 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'fern'
+//var bestCustomer;
+
+upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();
+}
+
+setBestCustomer = () => {
+    bestCustomer = 'not bob';
+}
+
+overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+}
+
+changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'bob :('
+}
